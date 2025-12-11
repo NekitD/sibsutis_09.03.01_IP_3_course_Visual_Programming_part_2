7bc96a9b4861e5a1954ca37293fd2c7541cf7ad4
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2025-12-11 21:29:20 +0700

    Prakt-5-6-7

---
+++ Prakt-5-6-7/build-qtabelo-Desktop_Qt_5_12_4_MinGW_64_bit-Debug/debug/moc_application_window.cpp
@@ -0,0 +1,340 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'application_window.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.4)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../qtabelo/application_window.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'application_window.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.4. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_ApplicationWindow_t {
+    QByteArrayData data[54];
+    char stringdata0[844];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_ApplicationWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_ApplicationWindow_t qt_meta_stringdata_ApplicationWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 17), // "ApplicationWindow"
+QT_MOC_LITERAL(1, 18, 20), // "documentCountChanged"
+QT_MOC_LITERAL(2, 39, 0), // ""
+QT_MOC_LITERAL(3, 40, 5), // "count"
+QT_MOC_LITERAL(4, 46, 12), // "openDocument"
+QT_MOC_LITERAL(5, 59, 3), // "url"
+QT_MOC_LITERAL(6, 63, 32), // "updateActionsDocumentTabPosition"
+QT_MOC_LITERAL(7, 96, 23), // "QTabWidget::TabPosition"
+QT_MOC_LITERAL(8, 120, 8), // "position"
+QT_MOC_LITERAL(9, 129, 32), // "updateActionsSheetTabBarPosition"
+QT_MOC_LITERAL(10, 162, 20), // "updateMenuOpenRecent"
+QT_MOC_LITERAL(11, 183, 15), // "documentCreated"
+QT_MOC_LITERAL(12, 199, 17), // "documentActivated"
+QT_MOC_LITERAL(13, 217, 14), // "QMdiSubWindow*"
+QT_MOC_LITERAL(14, 232, 9), // "subWindow"
+QT_MOC_LITERAL(15, 242, 25), // "documentTabVisibleChanged"
+QT_MOC_LITERAL(16, 268, 7), // "visible"
+QT_MOC_LITERAL(17, 276, 26), // "documentTabPositionChanged"
+QT_MOC_LITERAL(18, 303, 26), // "documentTabAutoHideChanged"
+QT_MOC_LITERAL(19, 330, 8), // "autoHide"
+QT_MOC_LITERAL(20, 339, 23), // "documentModifiedChanged"
+QT_MOC_LITERAL(21, 363, 8), // "modified"
+QT_MOC_LITERAL(22, 372, 18), // "documentUrlChanged"
+QT_MOC_LITERAL(23, 391, 14), // "documentClosed"
+QT_MOC_LITERAL(24, 406, 9), // "slotAbout"
+QT_MOC_LITERAL(25, 416, 12), // "slotColophon"
+QT_MOC_LITERAL(26, 429, 15), // "slotPreferences"
+QT_MOC_LITERAL(27, 445, 7), // "slotNew"
+QT_MOC_LITERAL(28, 453, 8), // "slotOpen"
+QT_MOC_LITERAL(29, 462, 8), // "slotSave"
+QT_MOC_LITERAL(30, 471, 10), // "slotSaveAs"
+QT_MOC_LITERAL(31, 482, 14), // "slotSaveCopyAs"
+QT_MOC_LITERAL(32, 497, 11), // "slotSaveAll"
+QT_MOC_LITERAL(33, 509, 12), // "slotCopyPath"
+QT_MOC_LITERAL(34, 522, 16), // "slotCopyFilename"
+QT_MOC_LITERAL(35, 539, 18), // "slotRenameFilename"
+QT_MOC_LITERAL(36, 558, 14), // "slotProperties"
+QT_MOC_LITERAL(37, 573, 14), // "slotCloseOther"
+QT_MOC_LITERAL(38, 588, 12), // "slotCloseAll"
+QT_MOC_LITERAL(39, 601, 19), // "slotToolButtonStyle"
+QT_MOC_LITERAL(40, 621, 14), // "const QAction*"
+QT_MOC_LITERAL(41, 636, 6), // "action"
+QT_MOC_LITERAL(42, 643, 18), // "slotToolButtonSize"
+QT_MOC_LITERAL(43, 662, 23), // "slotDocumentTabPosition"
+QT_MOC_LITERAL(44, 686, 19), // "slotShowSheetTabBar"
+QT_MOC_LITERAL(45, 706, 7), // "checked"
+QT_MOC_LITERAL(46, 714, 23), // "slotSheetTabBarPosition"
+QT_MOC_LITERAL(47, 738, 23), // "slotSheetTabBarAutoHide"
+QT_MOC_LITERAL(48, 762, 14), // "slotFullScreen"
+QT_MOC_LITERAL(49, 777, 16), // "importTableToCsv"
+QT_MOC_LITERAL(50, 794, 17), // "SimpleTableModel*"
+QT_MOC_LITERAL(51, 812, 5), // "table"
+QT_MOC_LITERAL(52, 818, 8), // "filename"
+QT_MOC_LITERAL(53, 827, 16) // "exportTableToCsv"
+
+    },
+    "ApplicationWindow\0documentCountChanged\0"
+    "\0count\0openDocument\0url\0"
+    "updateActionsDocumentTabPosition\0"
+    "QTabWidget::TabPosition\0position\0"
+    "updateActionsSheetTabBarPosition\0"
+    "updateMenuOpenRecent\0documentCreated\0"
+    "documentActivated\0QMdiSubWindow*\0"
+    "subWindow\0documentTabVisibleChanged\0"
+    "visible\0documentTabPositionChanged\0"
+    "documentTabAutoHideChanged\0autoHide\0"
+    "documentModifiedChanged\0modified\0"
+    "documentUrlChanged\0documentClosed\0"
+    "slotAbout\0slotColophon\0slotPreferences\0"
+    "slotNew\0slotOpen\0slotSave\0slotSaveAs\0"
+    "slotSaveCopyAs\0slotSaveAll\0slotCopyPath\0"
+    "slotCopyFilename\0slotRenameFilename\0"
+    "slotProperties\0slotCloseOther\0"
+    "slotCloseAll\0slotToolButtonStyle\0"
+    "const QAction*\0action\0slotToolButtonSize\0"
+    "slotDocumentTabPosition\0slotShowSheetTabBar\0"
+    "checked\0slotSheetTabBarPosition\0"
+    "slotSheetTabBarAutoHide\0slotFullScreen\0"
+    "importTableToCsv\0SimpleTableModel*\0"
+    "table\0filename\0exportTableToCsv"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_ApplicationWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      37,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  199,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       4,    1,  202,    2, 0x0a /* Public */,
+       6,    1,  205,    2, 0x08 /* Private */,
+       9,    1,  208,    2, 0x08 /* Private */,
+      10,    0,  211,    2, 0x08 /* Private */,
+      11,    0,  212,    2, 0x08 /* Private */,
+      12,    1,  213,    2, 0x08 /* Private */,
+      15,    1,  216,    2, 0x08 /* Private */,
+      17,    1,  219,    2, 0x08 /* Private */,
+      18,    1,  222,    2, 0x08 /* Private */,
+      20,    1,  225,    2, 0x08 /* Private */,
+      22,    1,  228,    2, 0x08 /* Private */,
+      23,    0,  231,    2, 0x08 /* Private */,
+      24,    0,  232,    2, 0x08 /* Private */,
+      25,    0,  233,    2, 0x08 /* Private */,
+      26,    0,  234,    2, 0x08 /* Private */,
+      27,    0,  235,    2, 0x08 /* Private */,
+      28,    0,  236,    2, 0x08 /* Private */,
+      29,    0,  237,    2, 0x08 /* Private */,
+      30,    0,  238,    2, 0x08 /* Private */,
+      31,    0,  239,    2, 0x08 /* Private */,
+      32,    0,  240,    2, 0x08 /* Private */,
+      33,    0,  241,    2, 0x08 /* Private */,
+      34,    0,  242,    2, 0x08 /* Private */,
+      35,    0,  243,    2, 0x08 /* Private */,
+      36,    0,  244,    2, 0x08 /* Private */,
+      37,    0,  245,    2, 0x08 /* Private */,
+      38,    0,  246,    2, 0x08 /* Private */,
+      39,    1,  247,    2, 0x08 /* Private */,
+      42,    1,  250,    2, 0x08 /* Private */,
+      43,    1,  253,    2, 0x08 /* Private */,
+      44,    1,  256,    2, 0x08 /* Private */,
+      46,    1,  259,    2, 0x08 /* Private */,
+      47,    1,  262,    2, 0x08 /* Private */,
+      48,    1,  265,    2, 0x08 /* Private */,
+      49,    2,  268,    2, 0x08 /* Private */,
+      53,    2,  273,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+
+ // slots: parameters
+    QMetaType::Bool, QMetaType::QUrl,    5,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 13,   14,
+    QMetaType::Void, QMetaType::Bool,   16,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, QMetaType::Bool,   19,
+    QMetaType::Void, QMetaType::Bool,   21,
+    QMetaType::Void, QMetaType::QUrl,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 40,   41,
+    QMetaType::Void, 0x80000000 | 40,   41,
+    QMetaType::Void, 0x80000000 | 40,   41,
+    QMetaType::Void, QMetaType::Bool,   45,
+    QMetaType::Void, 0x80000000 | 40,   41,
+    QMetaType::Void, QMetaType::Bool,   45,
+    QMetaType::Void, QMetaType::Bool,   45,
+    QMetaType::Bool, 0x80000000 | 50, QMetaType::QString,   51,   52,
+    QMetaType::Bool, 0x80000000 | 50, QMetaType::QString,   51,   52,
+
+       0        // eod
+};
+
+void ApplicationWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ApplicationWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->documentCountChanged((*reinterpret_cast< const int(*)>(_a[1]))); break;
+        case 1: { bool _r = _t->openDocument((*reinterpret_cast< const QUrl(*)>(_a[1])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 2: _t->updateActionsDocumentTabPosition((*reinterpret_cast< const QTabWidget::TabPosition(*)>(_a[1]))); break;
+        case 3: _t->updateActionsSheetTabBarPosition((*reinterpret_cast< const QTabWidget::TabPosition(*)>(_a[1]))); break;
+        case 4: _t->updateMenuOpenRecent(); break;
+        case 5: _t->documentCreated(); break;
+        case 6: _t->documentActivated((*reinterpret_cast< QMdiSubWindow*(*)>(_a[1]))); break;
+        case 7: _t->documentTabVisibleChanged((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 8: _t->documentTabPositionChanged((*reinterpret_cast< const QTabWidget::TabPosition(*)>(_a[1]))); break;
+        case 9: _t->documentTabAutoHideChanged((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 10: _t->documentModifiedChanged((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 11: _t->documentUrlChanged((*reinterpret_cast< const QUrl(*)>(_a[1]))); break;
+        case 12: _t->documentClosed(); break;
+        case 13: _t->slotAbout(); break;
+        case 14: _t->slotColophon(); break;
+        case 15: _t->slotPreferences(); break;
+        case 16: _t->slotNew(); break;
+        case 17: _t->slotOpen(); break;
+        case 18: _t->slotSave(); break;
+        case 19: _t->slotSaveAs(); break;
+        case 20: _t->slotSaveCopyAs(); break;
+        case 21: _t->slotSaveAll(); break;
+        case 22: _t->slotCopyPath(); break;
+        case 23: _t->slotCopyFilename(); break;
+        case 24: _t->slotRenameFilename(); break;
+        case 25: _t->slotProperties(); break;
+        case 26: _t->slotCloseOther(); break;
+        case 27: _t->slotCloseAll(); break;
+        case 28: _t->slotToolButtonStyle((*reinterpret_cast< const QAction*(*)>(_a[1]))); break;
+        case 29: _t->slotToolButtonSize((*reinterpret_cast< const QAction*(*)>(_a[1]))); break;
+        case 30: _t->slotDocumentTabPosition((*reinterpret_cast< const QAction*(*)>(_a[1]))); break;
+        case 31: _t->slotShowSheetTabBar((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 32: _t->slotSheetTabBarPosition((*reinterpret_cast< const QAction*(*)>(_a[1]))); break;
+        case 33: _t->slotSheetTabBarAutoHide((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 34: _t->slotFullScreen((*reinterpret_cast< const bool(*)>(_a[1]))); break;
+        case 35: { bool _r = _t->importTableToCsv((*reinterpret_cast< SimpleTableModel*(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 36: { bool _r = _t->exportTableToCsv((*reinterpret_cast< SimpleTableModel*(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 35:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< SimpleTableModel* >(); break;
+            }
+            break;
+        case 36:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< SimpleTableModel* >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (ApplicationWindow::*)(const int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&ApplicationWindow::documentCountChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject ApplicationWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_ApplicationWindow.data,
+    qt_meta_data_ApplicationWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *ApplicationWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ApplicationWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ApplicationWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ApplicationWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 37)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 37;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 37)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 37;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void ApplicationWindow::documentCountChanged(const int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
